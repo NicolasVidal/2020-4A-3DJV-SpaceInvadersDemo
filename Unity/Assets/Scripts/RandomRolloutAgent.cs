3408821caf03fe85a509cf8e5f46f97ac6b11e30
Nicolas Vidal <pro@nicolasvidal.fr>
2019-10-15 10:40:52 +0200

    First Burst Attempt, performance increased by x60 ! :)

--- Unity/Assets/Scripts/RandomRolloutAgent.cs
+++ Unity/Assets/Scripts/RandomRolloutAgent.cs
@@ -1,55 +1,92 @@
+using Unity.Burst;
 using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+using Random = Unity.Mathematics.Random;
 using Rules = SpaceInvadersGameStateRules;
 
 public class RandomRolloutAgent : IAgent
 {
-    public int Act(ref SpaceInvadersGameState gs, int[] availableActions)
+    [BurstCompile]
+    struct RandomRolloutJob : IJob
     {
-        var epochs = 10;
-        var agent = new RandomAgent();
-
-        var summedScores = new NativeArray<long>(availableActions.Length, Allocator.Temp);
-
-        for (var i = 0; i < availableActions.Length; i++)
+        public SpaceInvadersGameState gs;
+        
+        [ReadOnly]
+        public NativeArray<int> availableActions;
+        public RandomAgent rdmAgent;
+        
+        [WriteOnly]
+        public NativeArray<int> chosenAction;
+        
+        public void Execute()
         {
-            for (var n = 0; n < epochs; n++)
+            var epochs = 100;
+            var agent = rdmAgent;
+
+            var summedScores = new NativeArray<long>(availableActions.Length, Allocator.Temp);
+
+            var gsCopy = Rules.Clone(ref gs);
+            
+            for (var i = 0; i < availableActions.Length; i++)
             {
-                var gsCopy = Rules.Clone(ref gs);
-
-                Rules.Step(ref gsCopy, availableActions[i]);
-
-                var currentDepth = 0;
-                while (!gsCopy.isGameOver)
+                for (var n = 0; n < epochs; n++)
                 {
-                    Rules.Step(ref gsCopy, agent.Act(ref gsCopy, Rules.GetAvailableActions(ref gsCopy)));
-                    currentDepth++;
-                    if (currentDepth > 500)
+                    Rules.CopyTo(ref gs, ref gsCopy);
+                    Rules.Step(ref gsCopy, availableActions[i]);
+
+                    var currentDepth = 0;
+                    while (!gsCopy.isGameOver)
                     {
-                        break;
+                        Rules.Step(ref gsCopy, agent.Act(ref gsCopy, availableActions));
+                        currentDepth++;
+                        if (currentDepth > 500)
+                        {
+                            break;
+                        }
                     }
+
+                    summedScores[i] += gsCopy.playerScore;
+//                    gsCopy.enemies.Dispose();
+//                    gsCopy.projectiles.Dispose();
+                }
+            }
+
+            var bestActionIndex = -1;
+            var bestScore = long.MinValue;
+            for (var i = 0; i < summedScores.Length; i++)
+            {
+                if (bestScore > summedScores[i])
+                {
+                    continue;
                 }
 
-                summedScores[i] += gsCopy.playerScore;
-                gsCopy.enemies.Dispose();
-                gsCopy.projectiles.Dispose();
+                bestScore = summedScores[i];
+                bestActionIndex = i;
             }
-        }
 
-        var bestActionIndex = -1;
-        var bestScore = long.MinValue;
-        for (var i = 0; i < summedScores.Length; i++)
+//            summedScores.Dispose();
+
+            chosenAction[0] = availableActions[bestActionIndex];
+        }
+    }
+
+    public int Act(ref SpaceInvadersGameState gs, NativeArray<int> availableActions)
+    {
+        var job = new RandomRolloutJob
         {
-            if (bestScore > summedScores[i])
-            {
-                continue;
-            }
+            availableActions = availableActions,
+            gs = gs,
+            chosenAction = new NativeArray<int>(1, Allocator.TempJob),
+            rdmAgent = new RandomAgent{rdm = new Random((uint) Time.frameCount)}
+        };
 
-            bestScore = summedScores[i];
-            bestActionIndex = i;
-        }
-        
-        summedScores.Dispose();
+        var handle = job.Schedule();
+        handle.Complete();
 
-        return availableActions[bestActionIndex];
+        var chosenAction = job.chosenAction[0];
+
+        job.chosenAction.Dispose();
+        return chosenAction;
     }
 }
